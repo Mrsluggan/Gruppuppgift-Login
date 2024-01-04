657adc168737d70793a1be1539446b99ddba8248
Mrsluggan <ericflyger@gmail.com>
2024-01-04 11:33:27 +0100

    Grunden för startsidan

---
+++ src/main/java/com/LoginGrupp3/LoginGrupp3/controller/indexController.java
@@ -0,0 +1,14 @@
+package com.LoginGrupp3.LoginGrupp3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    @GetMapping
+    public String getIndex() {
+        return "index";
+    }
+
+}
