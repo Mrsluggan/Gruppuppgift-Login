82316426d92f324bad8cf347750d34ef4c4d98ff
Kribbz0r <kribbz0r@gmail.com>
2024-01-04 11:39:11 +0100

    .

---
+++ src/main/java/com/LoginGrupp3/LoginGrupp3/model/Car.java
@@ -0,0 +1,66 @@
+package com.LoginGrupp3.LoginGrupp3.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int price;
+    private String description;
+    private Object picture;
+
+    public Car(String name, int price, String description, Object picture) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.picture = picture;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Object getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Object picture) {
+        this.picture = picture;
+    }
+
+}
