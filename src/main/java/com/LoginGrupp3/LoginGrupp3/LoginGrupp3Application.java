adfdc1d1f3a55d5f090b4bf59ec2b42b8af823e0
Kribbz0r <kribbz0r@gmail.com>
2024-01-04 10:20:38 +0100

    .

---
+++ src/main/java/com/LoginGrupp3/LoginGrupp3/LoginGrupp3Application.java
@@ -0,0 +1,13 @@
+package com.LoginGrupp3.LoginGrupp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginGrupp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginGrupp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/LoginGrupp3/LoginGrupp3/LoginGrupp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LoginGrupp3.LoginGrupp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginGrupp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
