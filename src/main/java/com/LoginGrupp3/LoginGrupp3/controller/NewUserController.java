7928e1e430e2da5ea2bc2cb1e7f949877efbd8b2
Mrsluggan <ericflyger@gmail.com>
2024-01-05 09:28:46 +0100

    New User funktion

--- src/main/java/com/LoginGrupp3/LoginGrupp3/controller/NewUserController.java
+++ src/main/java/com/LoginGrupp3/LoginGrupp3/controller/NewUserController.java
@@ -1,27 +1,38 @@
 package com.LoginGrupp3.LoginGrupp3.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.LoginGrupp3.LoginGrupp3.model.User;
+import com.LoginGrupp3.LoginGrupp3.repository.UserRepository;
+
 @Controller
 public class NewUserController {
 
-@GetMapping("/createNewAccount")
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/createNewAccount")
     public String getNewUserForm() {
         return "createNewAccount";
     }
 
-@PostMapping ("/newUser")
-    public String newUser  (@RequestParam("firstName") String firstName,@RequestParam("surname") String surname,
-@RequestParam("email") String email, @RequestParam("username") String username, @RequestParam("password") String password) {
+    @PostMapping("/newUser")
+    public String newUser(@RequestParam("firstName") String firstName, @RequestParam("surname") String surname, @RequestParam("email") String email, @RequestParam("username") String username, @RequestParam("password") String password) {
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setSurname(surname);
+        user.setEmail(email);
+        user.setUsername(username);
+        user.setPassword(password);
+        userRepository.save(user);
         
-System.out.println(firstName + surname+email+username+password);
-return "redirect:/";
-}
-
-
-
+        System.out.println(firstName + surname + email + username + password);
+        return "redirect:/";
+    }
 
 }
