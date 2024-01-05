d74d642c41a802ca1f148fa72f0a02a29556e1cf
Kribbz0r <kribbz0r@gmail.com>
2024-01-05 13:40:00 +0100

    working with product description. not done

--- src/main/java/com/LoginGrupp3/LoginGrupp3/controller/indexController.java
+++ src/main/java/com/LoginGrupp3/LoginGrupp3/controller/indexController.java
@@ -1,5 +1,7 @@
 package com.LoginGrupp3.LoginGrupp3.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,17 +23,22 @@ public class indexController {
         return "index";
     }
 
-    @GetMapping ("/details")
-    public String getDetails(Model model) {
-        model.addAttribute("products", carRepository.findAll());
-        return "details";
-    }
-@GetMapping ("/details/{id}")
-public String getDetailsId(Model model, @PathVariable(value="id") int id) {
-    Car car =carRepository.findById(id).get();
-    model.addAttribute("product", car);
-    return "details/{id}";
-}
+    // Tog bort mappingen under. Konflikt om man har två som ska till samma ställe?
 
+    // @GetMapping("/details")
+    // public String getDetails(Model model) {
+    // model.addAttribute("products", carRepository.findAll());
+    // return "details";
+    // }
+
+    @GetMapping("/details/{id}")
+    public String getDetailsId(Model model, @PathVariable(value = "id") int id) {
+        System.out.println(id);
+        Optional<Car> car = carRepository.findById(id); // tog bort .get
+        model.addAttribute("product", car.get());
+        System.out.println("vad är detta?" + model);
+        System.out.println(car.get().getId());
+        return "details"; // tog bort /{id}
+    }
 
 }
