522bf6dbc172b89ec1eab5aeceffd6380d8e2cbe
ellenc91 <ellen.crona@live.se>
2024-01-05 11:39:28 +0100

    adds template details, mapping details

--- src/main/java/com/LoginGrupp3/LoginGrupp3/controller/indexController.java
+++ src/main/java/com/LoginGrupp3/LoginGrupp3/controller/indexController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.LoginGrupp3.LoginGrupp3.model.Car;
 import com.LoginGrupp3.LoginGrupp3.repository.CarRepository;
 
 @Controller
@@ -18,5 +20,18 @@ public class indexController {
         model.addAttribute("products", carRepository.findAll());
         return "index";
     }
+
+    @GetMapping ("/details")
+    public String getDetails(Model model) {
+        model.addAttribute("products", carRepository.findAll());
+        return "details";
+    }
+@GetMapping ("/details/{id}")
+public String getDetailsId(Model model, @PathVariable(value="id") int id) {
+    Car car =carRepository.findById(id).get();
+    model.addAttribute("product", car);
+    return "details/{id}";
+}
+
 
 }
