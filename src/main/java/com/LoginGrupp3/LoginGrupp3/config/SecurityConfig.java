120873328559c9ead9b1752dad049c91b4e3e113
Mrsluggan <ericflyger@gmail.com>
2024-01-10 11:26:00 +0100

    Lite smink på sidan

--- src/main/java/com/LoginGrupp3/LoginGrupp3/config/SecurityConfig.java
+++ src/main/java/com/LoginGrupp3/LoginGrupp3/config/SecurityConfig.java
@@ -4,13 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.LoginGrupp3.LoginGrupp3.service.JpsUserDetailService;
 
 @Configuration
+@EnableWebSecurity
+
 public class SecurityConfig {
 
     private final JpsUserDetailService jpsUserDetailService;
@@ -22,18 +26,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((request) -> request
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/login").permitAll()
-                .requestMatchers("/details/**").permitAll()
-                .requestMatchers("/orders").authenticated()
-                .requestMatchers("/createNewAccount").permitAll()
-                .requestMatchers("/image.png").permitAll()
-                .requestMatchers("/newUser").permitAll()
-                .requestMatchers("style.css").permitAll()
-                .requestMatchers("image.png").permitAll())
+        http
+
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/icons/**").permitAll()
+                        .requestMatchers("/login/**").permitAll()
+                        .requestMatchers("/details/**").permitAll()
+                        .requestMatchers("/orders").authenticated()
+                        .requestMatchers("/createNewAccount").permitAll()
+                        .requestMatchers("/image.png").permitAll()
+                        .requestMatchers("/newUser").permitAll()
+                        .requestMatchers("style.css").permitAll()
+                        .requestMatchers("image.png").permitAll())
+
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/"))
+
+
+                        
                 .userDetailsService(jpsUserDetailService)
-                .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
